9a642627937f9f8fa804405ec67c74509bba8cbd
Yujie Wu <allenwuyujie@gmail.com>
2020-12-29 17:22:44 +1100

    index.tsx can be transpiled to index.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+// JS 模块定义
+module.exports = {
+  // Declare the current mode, development mode or production mode
+  mode: 'production',
+  // The entry of the application, normally is './src'
+  entry: {
+    index: './lib/index.tsx'
+  },
+  // Output
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'), // 根据操作系统的不同，把输出路径定义，避免'\'问题
+    library: 'SUI', // 库的名字
+    libraryTarget: 'umd' // 输出格式，一般是umd，unify module dev
+  },
+  module: {
+    rules: [
+      // Set how to transpile .tsx
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader' // Transpile .tsx to .ts file
+      }
+    ]
+  }
+};
