a23bcd5ae2edf03ab795155b572ddc9758daf0fd
Yujie Wu <allenwuyujie@gmail.com>
2021-01-03 20:03:19 +1100

    added onClick event

--- lib/icon.component.tsx
+++ lib/icon.component.tsx
@@ -4,11 +4,12 @@ import './icon.styles.scss';
 
 interface IconProps {
   name: string;
+  onClick?: React.MouseEventHandler<SVGElement>;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="sui-icon">
+    <svg className="sui-icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon.component';
 
+const fn = () => {
+  console.log('1');
+};
+
 ReactDOM.render(
   <div>
     <Icon name="wechat" />
     <Icon name="qq" />
     <Icon name="alipay" />
-    <Icon name="google" />
+    <Icon name="google" onClick={fn} />
   </div>,
   document.getElementById('root')
 );
