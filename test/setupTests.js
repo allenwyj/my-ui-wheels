7bb22d671c1724392ac176cc6498ad2789a2e695
Yujie Wu <allenwuyujie@gmail.com>
2021-01-04 17:15:46 +1100

    enzyme for react 17

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,1 +1,7 @@
+const enzyme = require('enzyme');
+// TODO: udpate from official once Enzyme released version for React 17
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
+enzyme.configure({
+  adapter: new Adapter()
+});
