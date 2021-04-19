01239e850b641b9d7204cdc2ec54c9bb0df03490
Yujie Wu <allenwuyujie@gmail.com>
2021-04-19 14:40:37 +1000

    add two examples for dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,12 +3,40 @@ import Dialog from './dialog.component';
 
 export default function () {
   const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
+
   return (
-    <div>
-      <button onClick={() => setX(!x)}>click</button>
-      <Dialog visible={x}>
-        <div>hi</div>
-      </Dialog>
-    </div>
+    <>
+      <div>
+        <h1>Example 1</h1>
+        <button onClick={() => setX(!x)}>click</button>
+        <Dialog
+          visible={x}
+          buttons={[
+            <button onClick={() => setX(false)}>1</button>,
+            <button onClick={() => setX(false)}>2</button>,
+          ]}
+          onClose={() => setX(false)}
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+
+      <div>
+        <h1>Example 2</h1>
+        <button onClick={() => setY(!y)}>click</button>
+        <Dialog
+          visible={y}
+          buttons={[
+            <button onClick={() => setY(false)}>1</button>,
+            <button onClick={() => setY(false)}>2</button>,
+          ]}
+          onClose={() => setY(false)}
+          closeOnClickMask
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+    </>
   );
 }
