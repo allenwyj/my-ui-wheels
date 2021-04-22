606b001aa6ca59cb425d77f461670a2686acda10
Yujie Wu <allenwuyujie@gmail.com>
2021-04-22 16:06:58 +1000

    change layout if there is Aside direct children

--- lib/layout/layout.component.tsx
+++ lib/layout/layout.component.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { classNameMaker } from '../classes';
+import Aside from './aside.component';
 
 import './layout.styles.scss';
 
 // Accepting attributes such as style, className ...
-interface Props extends React.HTMLAttributes<HTMLElement> {}
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | Array<ReactElement>;
+}
 
 const prefixedClassname = classNameMaker('sui-layout');
 const pc = prefixedClassname;
 
 const Layout: React.FunctionComponent<Props> = (props) => {
   const { className, ...restProps } = props;
-  
+
+  // If Layout diretly has chidlren Aside, Layout's display direction will be change to row.
+  const childrenAsArray = props.children as Array<ReactElement>;
+  const hasAside =
+    childrenAsArray.length &&
+    (props.children as Array<ReactElement>).reduce(
+      (result, node) => result || node.type === Aside,
+      false
+    );
+
   return (
-    <div className={pc('', { extra: className })} {...restProps}>
+    <div
+      className={pc('', {
+        extra: [className, hasAside && 'hasAside'].join(' '),
+      })}
+      {...restProps}
+    >
       {props.children}
     </div>
   );
