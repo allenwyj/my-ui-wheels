38c72155c3e432c17ebe77048f11d385475767a9
Yujie Wu <allenwuyujie@gmail.com>
2021-04-22 14:55:53 +1000

    allow classNameMaker to take multiple class names

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -1,7 +1,15 @@
+interface Options {
+  extra: string | undefined;
+}
+
 function classNameMaker(prefix: string) {
-  return function (name?: string) {
+  return function (name?: string, options?: Options) {
     // filter if any element in the array is falsy value
-    return [prefix, name].filter(Boolean).join('-');
+    const result = [prefix, name].filter(Boolean).join('-');
+    if (options && options.extra) {
+      return [result, options.extra].filter(Boolean).join(' ');
+    }
+    return result;
   };
 }
 
