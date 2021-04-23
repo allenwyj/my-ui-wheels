6e46211a451c67692d11851a2cd3b5343800e4af
Yujie Wu <allenwuyujie@gmail.com>
2021-04-23 16:30:50 +1000

    refactor classNameMaker further

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -11,28 +11,22 @@ interface ClassToggles {
   [K: string]: boolean;
 }
 
-function classNameMaker(prefix: string) {
-  return function (name: string | ClassToggles, options?: Options) {
-    const nameInObject = typeof name === 'string' ? { [name]: name } : name;
-
-    // Pairing a name or multiple names with prefix.
-    const prefixedName = Object.entries(nameInObject)
-      // return the array which value is not false (so everything excepts false)
-      .filter((kv) => kv[1] !== false)
-      // take the name(s)
-      .map((kv) => kv[0])
-      // Pairing a name or multiple names with prefix individually
-      .map((name) => [prefix, name].filter(Boolean).join('-'))
-      // and join them together.
-      .join(' ');
-
+const classNameMaker = (prefix: string) => (
+  name: string | ClassToggles,
+  options?: Options
+) =>
+  // Pairing a name or multiple names with prefix.
+  Object.entries(typeof name === 'string' ? { [name]: name } : name)
+    // return the array which value is not false (so everything excepts false)
+    .filter((kv) => kv[1] !== false)
+    // take the name(s)
+    .map((kv) => kv[0])
+    // Pairing a name or multiple names with prefix individually
+    .map((name) => [prefix, name].filter(Boolean).join('-'))
     // Adding extra class into the result
-    if (options && options.extra) {
-      return [prefixedName, options.extra].filter(Boolean).join(' ');
-    }
-    return prefixedName;
-  };
-}
+    .concat((options && options.extra) || [])
+    // and join them together.
+    .join(' ');
 
 export { classNameMaker };
 
