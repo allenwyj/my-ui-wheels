c2e171ca2190a1d7eab2ee2a5cf60e2ec703a2b4
Yujie Wu <allenwuyujie@gmail.com>
2021-05-06 03:50:47 +1000

    add custom validator to support promise rules

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -3,6 +3,30 @@ import React, { useState } from 'react';
 import Form, { FormValue } from './form.component';
 import Validator, { noError } from './validator';
 
+// The existing usernames
+const usernames = ['allen', 'tom', 'jacky'];
+// Sitimulating an Ajax call.
+const checkUserName = (
+  username: string,
+  onSuccess: () => void,
+  onFailure: () => void
+) => {
+  setTimeout(() => {
+    console.log('setTimeout runs');
+
+    // username is NOT unique
+    if (usernames.indexOf(username) >= 0) {
+      console.log('Not unique');
+      onFailure();
+
+      // username is unique
+    } else {
+      console.log('Unique');
+      onSuccess();
+    }
+  }, 3000);
+};
+
 export default function () {
   const [formData, setFormData] = useState<FormValue>({
     username: '',
@@ -24,16 +48,33 @@ export default function () {
     const rules = [
       { key: 'username', required: true },
       { key: 'username', minLen: 8, maxLen: 16 },
+      {
+        key: 'username',
+        // Custom validator with a custom process of validation.
+        validator: {
+          name: 'unique',
+          validate(username: string) {
+            console.log('Calling custom validator.validate method.');
+            return new Promise<void>((resolve, reject) => {
+              checkUserName(username, resolve, reject);
+            });
+          },
+        },
+      },
       { key: 'username', pattern: /^[A-Za-z0-9]+$/ },
       { key: 'password', required: true },
     ];
 
     // Validating user inputs
-    const errors = Validator(formData, rules);
-    if (noError(errors)) {
-    } else {
-      setErrors(errors);
-    }
+    Validator(formData, rules, (errors) => {
+      console.log('All checks have been made.');
+      console.log(errors);
+      if (noError(errors)) {
+        // No error performed.
+      } else {
+        setErrors(errors);
+      }
+    });
   };
 
   return (
@@ -43,7 +84,7 @@ export default function () {
       errors={errors}
       buttons={
         <>
-          <Button level="primary" size="large" type="submit">
+          <Button level="primary" size="medium" type="submit">
             Submit
           </Button>
           <Button level="normal">Back</Button>
--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -6,12 +6,17 @@ interface FormRule {
   minLen?: number;
   maxLen?: number;
   pattern?: RegExp;
+  validator?: {
+    name: string;
+    validate: (value: string) => Promise<void>;
+  };
 }
 
 type FormRules = Array<FormRule>;
 
-interface FormErrors {
-  [K: string]: string[];
+interface ErrorInfo {
+  message: string;
+  promise?: Promise<any>;
 }
 
 function isEmpty(value: any) {
@@ -22,37 +27,97 @@ export function noError(errors: any) {
   return Object.keys(errors).length === 0;
 }
 
-const Validator = (formValue: FormValue, rules: FormRules): FormErrors => {
+const Validator = (
+  formValue: FormValue,
+  rules: FormRules,
+  onCallback: (errors: any) => void
+): void => {
   let errors: any = {};
 
-  const addError = (key: string, message: string) => {
+  // error can contain a promise.
+  // error object: {key1:[{message, promise?}, {}, {}, ...], key2: [], key3: [], ...}
+  const addError = (key: string, error: ErrorInfo) => {
     if (errors[key] === undefined) {
       errors[key] = [];
     }
-    errors[key].push(message);
+    errors[key].push(error);
   };
 
-  rules.map((rule) => {
-    const value = formValue[rule.key];
-    if (rule.required && isEmpty(value)) {
-      addError(rule.key, 'Required');
+  for (const rule of rules) {
+    const userInput = formValue[rule.key];
+
+    // If the custom validtor exists
+    if (rule.validator && rule.validator.validate) {
+      console.log('Has custom validator.');
+      const promise = rule.validator.validate(userInput);
+      addError(rule.key, { message: 'The username already exists.', promise });
     }
 
-    if (rule.minLen && !isEmpty(value) && value.length < rule.minLen) {
-      addError(rule.key, 'Too short');
+    if (rule.required && isEmpty(userInput)) {
+      addError(rule.key, { message: 'Required' });
     }
 
-    if (rule.maxLen && !isEmpty(value) && value.length > rule.maxLen) {
-      addError(rule.key, 'Too long');
+    if (rule.minLen && !isEmpty(userInput) && userInput.length < rule.minLen) {
+      addError(rule.key, { message: 'Too short' });
     }
 
-    if (rule.pattern && !isEmpty(value)) {
-      if (!rule.pattern.test(value)) {
-        addError(rule.key, 'Invalid pattern');
+    if (rule.maxLen && !isEmpty(userInput) && userInput.length > rule.maxLen) {
+      addError(rule.key, { message: 'Too long' });
+    }
+
+    if (rule.pattern && !isEmpty(userInput)) {
+      if (!rule.pattern.test(userInput)) {
+        addError(rule.key, { message: 'Invalid pattern' });
       }
     }
-  });
-  return errors;
+  }
+
+  const promiseList = flat(Object.values(errors))
+    .filter((el) => el.promise)
+    .map((el) => el.promise);
+
+  // Passing the errors object back when all asynchronous events are resolved/rejected.
+  Promise.all(promiseList).then(
+    () => {
+      // FIXME: Unique username will still get error message.
+      console.log('onFulfilled');
+      const newErrorsList = Object.keys(errors).map<[string, string[]]>(
+        (key) => [key, errors[key].map((el: ErrorInfo) => el.message)]
+      );
+      const newErrorsObj = fromEntries(newErrorsList);
+
+      onCallback(newErrorsObj);
+    },
+    () => {
+      console.log('onRejected');
+      // Getting the message from each error
+      const newErrorsList = Object.keys(errors).map<[string, string[]]>(
+        (key) => [key, errors[key].map((el: ErrorInfo) => el.message)]
+      );
+      const newErrorsObj = fromEntries(newErrorsList);
+
+      onCallback(newErrorsObj);
+    }
+  );
 };
 
 export default Validator;
+
+// flat() creates a new array with its sub-array elements.
+const flat = (inputArray: Array<any>) => {
+  const result = [];
+  for (const el of inputArray) {
+    el instanceof Array ? result.push(...el) : result.push(el);
+  }
+
+  return result;
+};
+
+const fromEntries = (inputArray: Array<[string, string[]]>) => {
+  const result: { [key: string]: string[] } = {};
+  for (const el of inputArray) {
+    result[el[0]] = el[1];
+  }
+
+  return result;
+};
